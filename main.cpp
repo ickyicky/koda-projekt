a14f71362e7e5e158f0bc07562724b3a8c6b8068
Doman <domanski@basement.pl>
2021-12-09 21:43:10 +0100

    tested linking

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 
+#include "src/lzss.hpp"
 
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		std::cout<<"Usage: koda compress|decompress FILE\n";
-		return 1;
-	}
+    if (argc != 3)
+        {
+            std::cout << "Usage: koda compress|decompress FILE\n";
+            return 1;
+        }
 
-	return 0;
+    return 0;
 }
