384cd5552d86f292d081b402047c6ad4f8ed4eec
Kornel Skorka <kornel.skorka@allegro.pl>
2022-01-27 18:10:58 +0100

    entropia danych wejściowych źródła blokowego rzędu 2 i 3

--- data/entropy.py
+++ data/entropy.py
@@ -1,37 +1,39 @@
-import unittest
 import matplotlib.image as mpimg
 from math import log2
 from collections import Counter
 from os import listdir
+import numpy as np
 
 
-def calculate_image_entropy(image_path):
+def calculate_image_entropy(image_path, block=1):
     image = mpimg.imread(image_path)
-    return _calculate_entropy(image.flatten())
+    return _calculate_entropy(image.flatten().astype('byte'), block)
 
 
-def _calculate_entropy(input_data):
+def _calculate_entropy(input_data, block=1):
+    input_data = _apply_blocks(input_data, block)
     freqs = Counter(input_data)
     probabilities = [float(freq) / len(input_data) for freq in freqs.values()]
     return -1.0 * sum(p * log2(p) for p in probabilities)
 
 
-name_to_entropy = [
-    (path.split('/')[1], calculate_image_entropy(path)) for path in (
-            ['obrazy/' + filename for filename in listdir("./obrazy")] +
-            ['rozklady/' + filename for filename in listdir("./rozklady")]
-    )
-]
-with open("entropy_results.txt", 'w') as writer:
-    for name, entropy in name_to_entropy:
-        writer.write(f"{name}: {entropy}\n")
+def _apply_blocks(data, blocks):
+    if blocks == 1:
+        return data
+    else:
+        result = []
+        for idx in range(0, len(data) - blocks + 1, 2):
+            result.append(sum(data[idx:(idx + blocks)]))
+        return result
 
-'''
-class TestEntropy(unittest.TestCase):
 
-    def test_entropy(self):
-        self.assertEqual(_calculate_entropy("aabbccdd"), 2.0)
-        self.assertEqual(_calculate_entropy("abcd"), 2.0)
-        self.assertEqual(_calculate_entropy("aaa"), 0.0)
-        self.assertEqual(_calculate_entropy("a"), 0.0)
-'''
+def calculate_name_to_entropy(paths):
+    return [(path.split('/')[1], calculate_image_entropy(path, 1), calculate_image_entropy(path, 2),calculate_image_entropy(path, 3)) for path in paths]
+
+
+paths = ['obrazy/' + filename for filename in listdir("./obrazy")] + ['rozklady/' + filename for filename in listdir("./rozklady")]
+name_to_entropy = calculate_name_to_entropy(paths)
+with open(f"entropy_results.csv", 'w') as writer:
+    writer.write("filename,1_block,2_blocks,3_blocks\n")
+    for name, e1, e2, e3 in name_to_entropy:
+        writer.write(f"{name},{e1},{e2},{e3}\n")
